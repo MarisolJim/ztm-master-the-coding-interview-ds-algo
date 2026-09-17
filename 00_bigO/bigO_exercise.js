a4639ace0941f99fde18710914579fdaa4b7c057
Marisol Jimenez <jimenezmarisol8884@gmail.com>
2026-09-16 19:43:46 -0700

    Update bigO_exercise.js

--- 00_bigO/bigO_exercise.js
+++ 00_bigO/bigO_exercise.js
@@ -1,13 +1,18 @@
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input) {
-    let a = 10;
-    a = 50 + 3;
+    let a = 10;//O(1)
+    a = 50 + 3;//O(1)
   
-    for (let i = 0; i < input.length; i++) {
-      anotherFunction();
-      let stranger = true;
-      a++;
+    for (let i = 0; i < input.length; i++) { //O(n)
+      anotherFunction();//O(n) - according to video answer because it is in the loop and going through all inputs
+      let stranger = true;//O(n)
+      a++;//O(n)
     }
-    return a;
+    return a;//O(1)
   }
-  
+  // the first part is constant as it is adding numbers
+  // a loop adds O(n) time so we are at O(n) + 2
+  // a function call adds time but I'm not sure if I can calculate the without knowing what the function does
+
+  //After watching video answer
+  //O(3 + 4n)
