c2dd5d6a9b515e006624bdc7b6f73fdf6a9e5406
Marisol Jimenez <jimenezmarisol8884@gmail.com>
2026-09-16 19:32:48 -0700

    Update findNemo2.js

--- 00_bigO/findNemo2.js
+++ 00_bigO/findNemo2.js
@@ -2,7 +2,7 @@
 const fish = ['dory', 'bruce', 'marlin', 'nemo'];
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(10).fill('nemo');
+const large = new Array(100).fill('nemo');
 
 function findNemo2(fish) {
   let t0 = performance.now();
@@ -16,3 +16,7 @@ function findNemo2(fish) {
 }
 
 findNemo2(everyone)
+// instead of using time to calcualte performance, we cam count the number fo operations the algorithm needs to compute
+// This is O(n) - we need to go through the entire array and check each element
+// If we have 10 names as inpute, we have to check all 10, hence O(n) - linear
+// one of the most common time complexities is O(n) - linear time complexity
